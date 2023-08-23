9f4acc140f7562f6319758547738e4c14135162d
SwethaVoora <swetha.voora01.com>
2023-08-23 17:11:17 -0400

    Creating Home Page

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,7 +4,7 @@ from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.http import JsonResponse
 
 # from django.http import HttpResponse
 
@@ -86,3 +86,14 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response(RoomSerializer(room).data, status=status.HTTP_400_BAD_REQUEST)
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key): 
+            self.request.session.create()
+
+        data = {
+            'code' : self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+    # Takes a Python dictionary and serializes it using a JSONserializer and sends it back to the frontend.
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,16 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Link, Route, Redirect, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, redirect, Routes, useNavigate, Navigate } from "react-router-dom";
 import Room from "./Room";
-
+import {Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 
 const HomePage = () => {
+
+  const [roomCode, setRoomCode] = useState(null);
+  // const navigate = useNavigate();
+
+  const RenderHomePage = () => {
+
+  
+    const getRoomCode = () => {
+      fetch('/api/user-in-room')
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+      
+    // After setting the roomCode, you can check and navigate if needed
+        // if (data.code != null) {
+        //   navigate('/room/' + data.code);
+        // }
+      });
+    }
+  
+    useEffect(() => {
+      getRoomCode();
+    }, []);
+  
+  
+    // fetch('/api/user-in-room')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setRoomCode(data.code);
+      
+    // // After setting the roomCode, you can check and navigate if needed
+    //     if (data.code != null) {
+    //       navigate('/room/' + data.code);
+    //     }
+    //   });
+  
+    // if(roomCode != null){
+      // navigate('/room/' + roomCode)
+    // }
+  
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant='h3' component='h3'>
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+
   return (
     // <h1> This is the Home Page.</h1>
     <Router>
     <Routes>
-      <Route path="/" element={<h1>This is the Home Page</h1>} />
+      <Route path="/" element={(roomCode == null) ? <RenderHomePage/> : (<Navigate to={`/room/${roomCode}`}/>)} />
+      {/* <Route path="/" element={<RenderHomePage/>} /> */}
       <Route path="/join" element={<RoomJoinPage/>} />
       <Route path="/create" element={<CreateRoomPage/>} />
       <Route path="/room/:roomCode" element={<Room/>}/>
